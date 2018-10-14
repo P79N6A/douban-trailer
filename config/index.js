57bc8fdb948e1e30c18c887fbd4e3ff16ddfd4bd
609457773@qq.com <609457773@qq.com>
2018-10-14 12:52:31 +0800

    all1

--- config/index.js
+++ config/index.js
@@ -1,8 +1,8 @@
 const AgentKeepAlive = require('agentkeepalive');
 module.exports = {
   'ossconfig': {
-    accessKeyId: 'LTAI7x3i2ioKHJVS',
-    accessKeySecret: 'WFbYPF9M9TK3yn8YIA5SccDv7NYr3n',
+    accessKeyId: '***',
+    accessKeySecret: '**',
     bucket: 'iuap-tenat-market',
     region: 'oss-cn-beijing',
     agent: new AgentKeepAlive({
